d4687d2ef781222381ef2f1ad82b984c1de636d2
nytschgeusen <nytsch@udk-berlin.de>
2018-06-01 14:39:05 +0200

    My first script

---
+++ Test_ArduinoIDE_HC-SR04.ino
@@ -0,0 +1,38 @@
+// defines pins numbers
+const int trigPin = 2; // ESP32-DevKit (Geekworm Easy Kit ESP32)
+const int echoPin = 5; // ESP32-DevKit (Geekworm Easy Kit ESP32)
+
+// defines variables
+long tempAir = 20.0; // Air temperature in degree Celsius
+long vAir; // sonic speed in m/s
+long duration; // sound wave travel time in microseconds
+int distance;
+
+void setup() {
+pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+Serial.begin(9600); // Starts the serial communication
+}
+
+void loop() {
+// Clears the trigPin
+digitalWrite(trigPin, LOW);
+delayMicroseconds(2);
+
+// Sets the trigPin on HIGH state for 10 micro seconds
+digitalWrite(trigPin, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPin, LOW);
+
+// Reads the echoPin, returns the sound wave travel time in microseconds
+duration = pulseIn(echoPin, HIGH); //
+
+// Calculating the distance
+vAir = 331.5 + (0.6 * tempAir); // in m/s
+distance = duration / 1e4 * vAir/2.0;
+
+// Prints the distance on the Serial Monitor
+Serial.print("Distance in cm: ");
+Serial.println(distance);
+delayMicroseconds(250000);
+}
